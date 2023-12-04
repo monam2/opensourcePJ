11d64c92f7dbd62515b57efeba302858c6ff2770
모남 <138136517+monam2@users.noreply.github.com>
2023-12-04 19:56:39 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from googletrans import Translator
 # easyocr 독일어(de)로 설정
 reader = easyocr.Reader(['de'])
 
-# 구글 번역 라이브러리
+# 구글 trans 선언 및 초기화
 translator = Translator()
 
 # 웹캠 연동 cap-> cv2.VideoCapture(0)
@@ -15,30 +15,28 @@ cap = cv2.VideoCapture(0)
 desired_fps = 60
 cap.set(cv2.CAP_PROP_FPS, desired_fps)
 
-while True: # 루프 -> 프레임
-    # 웹캠에서 프레임을 읽습니다. read함수는 성공적으로 프레임을 읽었는지를 나타내는 bool값 ret과 읽은 프레임 frame을 반환합니다.
+while True: # 루프 -> 프레임 단위
+    # 웹캠에서 프레임을 읽습니다. read함수는 성공적으로 프레임을 읽었는지를 나타내는 bool값 ret과 읽은 프레임 frame을 반환
+    # 프레임이 정상 로드 -> ret : True, frame : 단위 프레임
     ret, frame = cap.read()
-    if not ret: # 만약 프레임 읽기가 실패하면 루프를 종료합니다.
+    if not ret: # 프레임x -> 루프를 종료
         break
 
-    # easyocr을 사용하여 이미지에서 텍스트를 추출합니다.
+    # easyocr 텍스트 추출(
     result = reader.readtext(frame)
 
-    # 추출된 텍스트가 있을 때만 독일어로 판단하고 번역합니다.
+    # 텍스트가 있으면 독일어 문자열로 받아와 출력
     if result:
-        detected_text = result[0][1]  # 첫 번째로 감지된 텍스트만 사용합니다. 이는 단순히 코드를 간단하게 유지하기 위한 것입니다.
-
-        # 번역된 텍스트를 화면에 출력합니다.
+        detected_text = result[0][1]
         print("Detected Text (German):", detected_text)
 
 
-    # 웹캠 비디오 창에 프레임을 표시합니다. 실시간으로 어떤 이미지를 캡처하고 있는지 확인할 수 있게 해줍니다.
+    # 실시간 웹캠 영상 출력(프레임 단위)
     cv2.imshow('WebCam', frame)
 
-    # 'q' 키를 누르면 루프에서 빠져나옵니다.
+    # 'q' 키 누르면 종료
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# 웹캠을 닫습니다.
 cap.release()
-cv2.destroyAllWindows() # 모든 opencv창을 닫습니다.
+cv2.destroyAllWindows()
