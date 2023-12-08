3f990506cb13a2cf9e8d5a8c8eed4d649c8c1f76
모남 <138136517+monam2@users.noreply.github.com>
2023-12-08 23:46:00 +0900

    Update test.py by ddoggangi
    Delete create2 and Update test.py

--- test.py
+++ test.py
@@ -2,39 +2,28 @@ import cv2
 import easyocr
 from googletrans import Translator
 
-# easyocr 독일어(de)로 설정
 reader = easyocr.Reader(['de'])
-
-# 구글 trans 선언 및 초기화
 translator = Translator()
-
-# 웹캠 연동 cap-> cv2.VideoCapture(0)
 cap = cv2.VideoCapture(0)
-
-# 프레임 설정 : 60
 desired_fps = 60
 cap.set(cv2.CAP_PROP_FPS, desired_fps)
 
-while True: # 루프 -> 프레임 단위
-    # 웹캠에서 프레임을 읽습니다. read함수는 성공적으로 프레임을 읽었는지를 나타내는 bool값 ret과 읽은 프레임 frame을 반환
-    # 프레임이 정상 로드 -> ret : True, frame : 단위 프레임
+while True:
     ret, frame = cap.read()
-    if not ret: # 프레임x -> 루프를 종료
+    if not ret:
         break
 
-    # easyocr 텍스트 추출(
     result = reader.readtext(frame)
 
-    # 텍스트가 있으면 독일어 문자열로 받아와 출력
     if result:
         detected_text = result[0][1]
+        translated_text = translator.translate(detected_text, src='de', dest='ko').text
+
         print("Detected Text (German):", detected_text)
+        print("Translated Text (Korean):", translated_text)
 
-
-    # 실시간 웹캠 영상 출력(프레임 단위)
     cv2.imshow('WebCam', frame)
 
-    # 'q' 키 누르면 종료
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
